e327e705fc73f8dd9223e3f145332fc3a0c9ddc5
titzko <matizko@gmail.com>
2023-05-07 09:22:44 +0200

    .

--- src/main/java/com/example/nginx_ssi/controller/SampleController.java
+++ src/main/java/com/example/nginx_ssi/controller/SampleController.java
@@ -1,2 +1,17 @@
-package com.example.nginx_ssi.controller;public class SampleController {
+package com.example.nginx_ssi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+
+
+    @GetMapping
+    public String getSample() {
+        return "sample";
+    }
 }
